d1033beeb1f5777bdee12adfcd4a2989e56f2cec
deepikaparasa6 <deepikaparasa6@gmail.com>
2026-09-01 20:50:29 +0530

    Constant Time Complexity - O(1)

---
+++ complexity/bigo1.py
@@ -0,0 +1,6 @@
+def add_items(n):
+    return n + n
+
+print(add_items(10))
+''' here the time complexity is O(1) because the function performs 
+    a constant time operation (addition) regardless of the input size n.'''
