671dcf16323d0f15d2f0d95377d649ce2f2d96f9
deepikaparasa6 <deepikaparasa6@gmail.com>
2026-09-07 20:50:28 +0530

    Class example

--- classes/classes.py
+++ classes/classes.py
@@ -3,10 +3,11 @@ class Cookie:
         self.color = color 
     def get_color(self):
         return self.color
-    def set_color(self):
+    def set_color(self, color):
         self.color = color
 Cookie_one = Cookie('green')
 Cookie_two = Cookie('blue')
 print("Cookie one color: ",Cookie_one.get_color()) #output: Cookie one color:  green
 print("Cookie two color: ",Cookie_two.get_color()) #output: Cookie two color:  blue
-
+Cookie_one.set_color('red')
+print("Cookie one color: ",Cookie_one.get_color()) #output: Cookie one color:  red
