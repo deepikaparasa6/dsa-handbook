53fc794bcbeaba6702bdd4da4d3289b4b7534d91
deepikaparasa6 <deepikaparasa6@gmail.com>
2026-09-07 20:52:49 +0530

    Class example

--- classes/classes.py
+++ classes/classes.py
@@ -1,11 +1,11 @@
-class Cookie:
-    def __init__(self,color): 
+class Cookie: #this is class
+    def __init__(self,color): #this is constructor method
         self.color = color 
     def get_color(self):
         return self.color
     def set_color(self, color):
         self.color = color
-Cookie_one = Cookie('green')
+Cookie_one = Cookie('green') #cookie_one is an object of class Cookie and calliing constructor method
 Cookie_two = Cookie('blue')
 print("Cookie one color: ",Cookie_one.get_color()) #output: Cookie one color:  green
 print("Cookie two color: ",Cookie_two.get_color()) #output: Cookie two color:  blue
